a531d2c87290406af53287842a11c35040e24a1b
levykohout <levy.kohout@yahoo.com>
2016-12-12 20:25:36 -0600

    moved admin ajax calls to admin service file

--- public/scripts/admin.service.js
+++ public/scripts/admin.service.js
@@ -1,58 +1,73 @@
 angular.module('blueWatchApp')
-    .factory('adminservice', function($http){
+    .service('adminservice', function($http) {
 
-  var adminservice = this;
-  //this is to use logged in user in view
-  adminservice.user = "";
-  adminservice.isLoggedIn = false;
-  adminservice.accessLevel=false;
+        var adminservice = this;
+        //this is to use logged in user in view
+        adminservice.user = "";
+        adminservice.isLoggedIn = false;
+        adminservice.accessLevel = false;
 
-    adminservice.loggedin = function(){
-      return $http.get('/admin/adminSchema').then(function(response) {
-        adminservice.user = response.data.firstName;
-        console.log(response.data);
+        adminservice.loggedin = function() {
+            return $http.get('/admin/adminSchema').then(function(response) {
+                adminservice.user = response.data.firstName;
+                adminservice.isLoggedIn = true;
+                if (response.data.accessLevel == 'no') {
+                    adminservice.accessLevel = false;
+                } else {
+                    adminservice.accessLevel = true;
+                }
 
-        adminservice.isLoggedIn = true;
-        if (response.data.accessLevel == 'no'){
-            adminservice.accessLevel = false;
-        } else {
-            adminservice.accessLevel = true;
-        }
-        // console.log(response.data.accessLevel);
-        // console.log('adminservice user', adminservice.user);
-        // console.log('isLoggedIn ', adminservice.isLoggedIn);
-        return adminservice.user;
+                return adminservice.user;
 
-      },function(error){
-        adminservice.isLoggedIn = true; //look here
-        // console.log('isLoggedIn ', adminservice.isLoggedIn);
-        return false;
-      }
-    );
-    };
+            }, function(error) {
+
+                return adminservice.isLoggedIn = false;
+            });
+        };
 
 
-    adminservice.normalLoggedin = function(){
-      return $http.get('/login/info').then(function(response) {
-          console.log(response.data);
-        adminservice.user = response.data.firstName;
-        adminservice.isLoggedIn = true;
-        if (response.data.accessLevel == 'no'){
-            adminservice.accessLevel = false;
-        } else {
-            adminservice.accessLevel = true;
-        }
-        // console.log(response.data.accessLevel);
-        // console.log('adminservice user regular', adminservice.user);
-        // console.log('isLoggedIn ', adminservice.isLoggedIn);
-        return adminservice.user;
+        adminservice.normalLoggedin = function() {
+            return $http.get('/login/info').then(function(response) {
+                adminservice.user = response.data.firstName;
+                adminservice.isLoggedIn = true;
+                if (response.data.accessLevel == 'no') {
+                    adminservice.accessLevel = false;
+                } else {
+                    adminservice.accessLevel = true;
+                }
 
-      },function(error){
-        adminservice.isLoggedIn = true; //look here
-        // console.log('isLoggedIn ', adminservice.isLoggedIn);
-        return false;
-      }
-    );
-    };
-    return this;
-});
+                return adminservice.user;
+
+            }, function(error) {
+
+                return adminservice.isLoggedIn = false;
+            });
+        };
+
+        //get admin users
+        adminservice.getUsers = function() {
+            return $http.get('/admin').then(function(response) {
+                return response;
+            });
+        }; //End of get admin users
+
+        //add new user
+        adminservice.addNewUser = function(userData) {
+            return $http.post('/admin', userData).then(function(response) {
+                return response;
+            });
+        }; //End of addNewUser
+
+        adminservice.updateUser = function(id, userData) {
+            return $http.put('/admin/' + id, userData).then(function(response) {
+                return response;
+            });
+        }; //End of updateUser
+
+        adminservice.deleteUser = function(id) {
+            return $http.delete('/admin/' + id).then(function(response) {
+                return response;
+            });
+        }; //End of deleteUser
+
+    }); //End of service function
--- public/scripts/adminSetup.controller.js
+++ public/scripts/adminSetup.controller.js
@@ -1,101 +1,96 @@
 angular.module('blueWatchApp')
-.controller('AdminSetupController', AdminSetupController);
+    .controller('AdminSetupController', AdminSetupController);
 
 function AdminSetupController($http, adminservice) {
-console.log('AdminSetupController loaded!');
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();
-});
-var admin = this;
-admin.usersArray = [];
-admin.capturedId = '';
-//sets logged in user info
-admin.accessLevel= 'no';
-admin.adminservice = adminservice;
-
-admin.adminservice.loggedin();
-//whenever controller is loaded, will check to see if user which/if any user is logged in
-
-admin.getUsers = function(){
-    $http.get('/admin').then(function(response){
-        console.log('successfully get users',response);
-        admin.usersArray = response.data;
-
+    console.log('AdminSetupController loaded!');
+    $(document).ready(function() {
+        $('[data-toggle="popover"]').popover();
     });
-}//End of getUser
+    var admin = this;
+    admin.usersArray = [];
+    admin.capturedId = '';
+    admin.accessLevel = 'no';
+    admin.adminservice = adminservice;
 
-admin.addNewUser = function(firstName, lastName, email, password, access){
 
-    var data = {
-        firstName:firstName,
-        lastName:lastName,
-        email:email,
-        password:password,
-        accessLevel: admin.accessLevel
-    };
-// console.log(data);
-$http.post('/admin', data).then(function(response){
-    console.log('successfully added a new user', response);
+    //whenever controller is loaded, will check to see if user which/if any user is logged in
+    admin.adminservice.loggedin();
+
+    admin.getUsers = function() {
+            admin.adminservice.getUsers().then(function(response) {
+                console.log('successfully get users', response);
+                admin.usersArray = response.data;
+            });
+        } //End of getUser
+
+    admin.addNewUser = function(firstName, lastName, email, password, access) {
+
+        var data = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            accessLevel: admin.accessLevel
+        };
+
+        admin.adminservice.addNewUser(data).then(function(response) {
+            console.log('successfully added a new user', response);
+            admin.getUsers();
+            // empty form
+            admin.firstName = '';
+            admin.lastName = '';
+            admin.email = '';
+            admin.password = '';
+            admin.accessLevel = 'no';
+        });
+
+    }; //End of addNewUser
+
+
+
     admin.getUsers();
-    // empty form
-    admin.firstName='';
-    admin.lastName='';
-    admin.email='';
-    admin.password='';
-    admin.accessLevel='no';
-});
 
+//capturen info on modal click
+    admin.captureInfo = function(id, firstName, lastName, email, password, access) {
+        admin.capturedId = id;
+        admin.capturedFirstName = firstName;
+        admin.capturedLastName = lastName;
+        admin.capturedEmail = email;
+        admin.capturedPassword = password;
+        admin.capturedAccessLevel = access;
+    }; //End of captureId
 
-}; //End of addNewUser
+    admin.updateUser = function(firstName, lastName, email, password, access) {
+        var data = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            accessLevel: access
+        };
+        admin.adminservice.updateUser(admin.capturedId, data).then(function(response) {
+            console.log('successfully updated the user', response);
+            admin.getUsers();
 
+            // empty form
+            admin.firstName = '';
+            admin.lastName = '';
+            admin.email = '';
+            admin.password = '';
+            admin.accessLevel = 'no';
 
-
-admin.getUsers();
-
-admin.captureInfo = function(id, firstName, lastName, email, password, access){
-admin.capturedId=id;
-admin.capturedFirstName=firstName;
-admin.capturedLastName=lastName;
-admin.capturedEmail=email;
-admin.capturedPassword=password;
-admin.capturedAccessLevel=access;
-console.log(password);
-
-console.log('capturedId ', admin.capturedId);
-};//End of captureId
-
-admin.updateUser = function(firstName, lastName, email, password,access){
-    var data = {
-        firstName:firstName,
-        lastName:lastName,
-        email:email,
-        password:password,
-        accessLevel: access
-    };
-    console.log(data);
-    $http.put('/admin/'+ admin.capturedId, data).then(function(response){
-        console.log('successfully updated the user', response);
-        admin.getUsers();
-
-        // empty form
-        admin.firstName='';
-        admin.lastName='';
-        admin.email='';
-        admin.password='';
-        admin.accessLevel='no';
-
-    });
-}; //End of updateUser
+        });
+    }; //End of updateUser
 
 
 
-admin.deleteUser = function(id){
+    admin.deleteUser = function(id) {
 
-    $http.delete('/admin/'+id).then(function(response){
-        console.log('successfully deleted user', response);
-        admin.getUsers();
-    });
-}; //End of deleteUser
+        admin.adminservice.deleteUser(id).then(function(response) {
+            console.log('successfully deleted the user', response);
+            admin.getUsers();
+        });
+    }; //End of deleteUser
 
 
 
